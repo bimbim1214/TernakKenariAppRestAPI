6bb759aeb45077ce7f24564dd86017735872ec62
bimbim1214 <bimoadityapangestu@gmail.com>
2025-06-06 04:36:16 +0700

    Tambah model untuk daftar dan detail burung berdasarkan ID

--- lib/data/model/response/burung_semua_tersedia_model.dart
+++ lib/data/model/response/burung_semua_tersedia_model.dart
@@ -45,3 +45,44 @@ class BurungSemuaTersediaModel {
     "data": List<dynamic>.from(data.map((x) => x.toJson())),
   };
 }
+
+class BurungSemuaTersediabyIdModel {
+  final String message;
+  final int statusCode;
+  final DataBurungTersedia data;
+
+  BurungSemuaTersediabyIdModel({
+    required this.message,
+    required this.statusCode,
+    required this.data,
+  });
+
+  BurungSemuaTersediabyIdModel copyWith({
+    String? message,
+    int? statusCode,
+    DataBurungTersedia? data,
+  }) => BurungSemuaTersediabyIdModel(
+    message: message ?? this.message,
+    statusCode: statusCode ?? this.statusCode,
+    data: data ?? this.data,
+  );
+
+  factory BurungSemuaTersediabyIdModel.fromRawJson(String str) =>
+      BurungSemuaTersediabyIdModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory BurungSemuaTersediabyIdModel.fromJson(Map<String, dynamic> json) =>
+      BurungSemuaTersediabyIdModel(
+        message: json["message"],
+        statusCode: json["status_code"],
+        data: DataBurungTersedia.fromJson(json["data"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": data.toJson(),
+  };
+}
+
