00cc1a51d6760331c1e7a63f88f45463c02bcd18
bimbim1214 <bimoadityapangestu@gmail.com>
2025-06-06 00:58:49 +0700

    konversi JSON untuk IndukRequestModel

--- lib/data/model/request/admin/induk_request_model.dart
+++ lib/data/model/request/admin/induk_request_model.dart
@@ -17,5 +17,25 @@ class IndukRequestModel {
         this.gambarInduk,
     });
 
+    factory IndukRequestModel.fromJson(String str) => IndukRequestModel.fromMap(json.decode(str));
 
+    String toJson() => json.encode(toMap());
+
+    factory IndukRequestModel.fromMap(Map<String, dynamic> json) => IndukRequestModel(
+        noRing: json["no_ring"],
+        tanggalLahir: json["tanggal_lahir"] == null ? null : DateTime.parse(json["tanggal_lahir"]),
+        jenisKelamin: json["jenis_kelamin"],
+        jenisKenari: json["jenis_kenari"],
+        keterangan: json["keterangan"],
+        gambarInduk: json["gambar_induk"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "no_ring": noRing,
+        "tanggal_lahir": "${tanggalLahir!.year.toString().padLeft(4, '0')}-${tanggalLahir!.month.toString().padLeft(2, '0')}-${tanggalLahir!.day.toString().padLeft(2, '0')}",
+        "jenis_kelamin": jenisKelamin,
+        "jenis_kenari": jenisKenari,
+        "keterangan": keterangan,
+        "gambar_induk": gambarInduk,
+    };
 }
