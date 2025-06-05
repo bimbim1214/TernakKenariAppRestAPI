5adc13739aeb8a5ef873e47d8887506e1017bc34
bimbim1214 <bimoadityapangestu@gmail.com>
2025-06-06 00:45:42 +0700

    Tambah model AdminProfile dan konversi JSON

--- lib/data/model/response/admin_profile_response_model.dart
+++ lib/data/model/response/admin_profile_response_model.dart
@@ -28,3 +28,26 @@ class AdminProfileResponseModel {
     };
 }
 
+class AdminProfile {
+    final int? id;
+    final String? name;
+
+    AdminProfile({
+        this.id,
+        this.name,
+    });
+
+    factory AdminProfile.fromJson(String str) => AdminProfile.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory AdminProfile.fromMap(Map<String, dynamic> json) => AdminProfile(
+        id: json["id"],
+        name: json["name"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+    };
+}
