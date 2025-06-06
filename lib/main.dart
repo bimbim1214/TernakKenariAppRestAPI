dc002d15b97918191ce6a73fb750416330bb2247
bimbim1214 <bimoadityapangestu@gmail.com>
2025-06-06 22:09:20 +0700

    Inisialisasi MultiBlocProvider untuk autentikasi dan data burung

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tekukur_app/data/repository/auth_repository.dart';
+import 'package:tekukur_app/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:tekukur_app/data/repository/profile_buyer_repository.dart';
+import 'package:tekukur_app/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:tekukur_app/presentation/auth/bloc/register/register_bloc.dart';
+import 'package:tekukur_app/presentation/auth/login_screen.dart';
+import 'package:tekukur_app/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_bloc.dart';
+import 'package:tekukur_app/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
+import 'package:tekukur_app/services/service_http_client.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,27 +20,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => 
+          LoginBloc(authRepository: AuthRepository(ServiceHttpClient())),
+        ),
+        BlocProvider(
+          create: (context) => 
+          RegisterBloc(authRepository: AuthRepository(ServiceHttpClient())),
+        ),
+        BlocProvider(
+          create: (context) => 
+          ProfileBuyerBloc(profileBuyerRepository: ProfileBuyerRepository(ServiceHttpClient())),
+        ),
+        BlocProvider(
+          create: (context) =>
+          GetBurungTersediaBloc(GetAllBurungTersediaRepository(ServiceHttpClient())),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Tekukur App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const LoginScreen(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
