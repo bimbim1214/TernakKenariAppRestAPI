47375c9cd76592bd352f100ce6f50d9b5ee50da4
bimbim1214 <bimoadityapangestu@gmail.com>
2025-06-06 19:06:53 +0700

    Implementasi fungsi login dan register di AuthRepository

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:tekukur_app/data/model/request/auth/login_request_model.dart';
+import 'package:tekukur_app/data/model/request/auth/register_request_model.dart';
+import 'package:tekukur_app/data/model/response/auth_response_model.dart';
 import 'package:tekukur_app/services/service_http_client.dart'; // Adjust the import according to your project structure
 
 class AuthRepository {
@@ -6,5 +13,58 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._serviceHttpClient);
-  
+
+  Future<Either<String, AuthResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "login",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = AuthResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: "authToken",
+          value: loginResponse.user!.token,
+        );
+        await secureStorage.write(
+          key: "userRole",
+          value: loginResponse.user!.role,
+        );
+        log("Login successful: ${loginResponse.message}");
+        return Right(loginResponse);
+      } else {
+        log("Login failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Login failed");
+      }
+    } catch (e) {
+      log("Error in login: $e");
+      return Left("An error occurred while logging in.");
+    }
+  }
+
+  Future<Either<String, String>> register(
+    RegisterRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "register",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final registerResponse = jsonResponse['message'] as String;
+        log("Registration successful: ${registerResponse}");
+        return Right(registerResponse);
+      } else {
+        log("Registration failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Registration failed");
+      }
+    } catch (e) {
+      log("Error in registration: $e");
+      return Left("An error occurred while registering.");
+    }
+  }
 }
